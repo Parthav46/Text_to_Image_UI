8a2155fc480865546f74554c92c4077d21a7b274
Parthav46 <parthav46@gmail.com>
2020-05-24 22:18:48 +0530

    initial commit

---
+++ Text_to_Image_UI/views.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+import numpy as np
+from django.shortcuts import render
+
+def index(request):
+    model = tf.keras.Sequential([tf.keras.layers.Dense(units=1, input_shape=[2])])
+    model.compile(optimizer='sgd', loss='mean_squared_error')
+    model.load_weights('static/sample.h5')
+    val = model.predict([[float(request.GET['a']), float(request.GET['b'])]])
+    context = {
+        'ans': val
+    }
+    return render(request, 'index.html', context)
