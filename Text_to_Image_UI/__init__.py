211beb8fe4616d3d81a538a959fbacc38d43b064
Parthav46 <parthav46@gmail.com>
2020-06-06 03:25:41 +0530

    cache clear added

--- Text_to_Image_UI/__init__.py
+++ Text_to_Image_UI/__init__.py
@@ -1,6 +1,10 @@
 import os
+from django.core.cache import cache
 
 # setting up Environment
 image_dir = os.path.join('./static/images')
 if not os.path.exists(image_dir):
 	os.mkdir(image_dir)
+
+# clearing Cache
+cache.clear()
